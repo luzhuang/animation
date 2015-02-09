4f792166f1167eb631b3237db619a0fa73c403ca
陆庄 <wz84888@alibaba-inc.com>
2015-02-09 21:27:40 +0800

    aaa

--- js/animation.js
+++ js/animation.js
@@ -11,13 +11,13 @@
 		           };
  		})();
 
- 		function Sprite(src,x,y,width,height,action){
- 			this.x = x;
- 			this.y = y;
- 			this.width = width;	
- 			this.height = height;
+ 		function Sprite(img,x,y,width,height,action){
+ 			this.x = this.originX = x;
+ 			this.y = this.originY = y;
+ 			this.width = this.originWidth = width;	
+ 			this.height = this.originHeight = height;
  			this.size = Math.sqrt(width*width+height*height);
- 			this.src = src;
+ 			this.img = img;
  			this.action = action;
  			this.isTurn = false;
  			this.stage = {};
@@ -37,12 +37,12 @@
  					width = this.width,
  					height = this.height,
  					size = this.size;
- 				ctx.translate(this.size/2,this.size/2);
- 				var img = new Image();
- 				img.src = this.src;
- 				this.img = img;
-				img.onload = function(){
+					img = this.img;
+ 				if (this.action.name != 'stay') {
+	 				ctx.translate(this.size/2,this.size/2);
 					ctx.drawImage(img,-width/2,-height/2,width,height);
+				}else{
+					ctx.drawImage(img,0,0,width,height);
 				}
  			},
  			move : function() {
@@ -72,9 +72,30 @@
 					ctx.rotate(action.angle*Math.PI/180);
 					ctx.drawImage(this.img,-width/2,-height/2,width,height);
 				}
+				if (/bezier/i.test(actionName)){
+					var x = this.x,
+						y = this.y,
+						action = this.action;
+						// console.log(1);
+					this.action.t += 1/60;
+					console.log(action.p2.y);
+					this.x = Math.pow((1-action.t),3)*x+3*action.t*Math.pow((1-action.t),2)*(x+action.p1.x)+3*Math.pow(action.t,2)*(1-action.t)*(x+action.p2.x)+(x+action.p3.x)*Math.pow(action.t,3);
+					this.y = Math.pow((1-action.t),3)*y+3*action.t*Math.pow((1-action.t),2)*(y+action.p1.y)+3*Math.pow(action.t,2)*(1-action.t)*(y+action.p2.y)+(y+action.p3.y)*Math.pow(action.t,3);
+					if (this.action.t > 1 || this.y>this.stage.height) {
+						// console.log(this.action.t,this.stage.height);
+						this.action.t = 0;
+					}
+				}
 				if (/turnY/i.test(actionName)){
 					if (!this.isTurn) {
 						this.turn = this._turnY();
+						this.isTurn = true;
+					}
+					this.turn();
+				}
+				if (/turnX/i.test(actionName)){
+					if (!this.isTurn) {
+						this.turn = this._turnX();
 						this.isTurn = true;
 					}
 					this.turn();
@@ -102,15 +123,47 @@
 					ctx.drawImage(img,-width/2,-height/2,width,height);
 				});
  			},
+ 			_turnX : function(){
+				var action = this.action,
+	 				width = this.width,
+	 				height = this.height,
+	 				size = this.size,
+	 				ctx = this.ctx,
+	 				img = this.img;
+	 			var scale = 1;
+ 				return (function(){
+ 					scale *= action.turn;
+	 				ctx.clearRect(-size/2,-size/2,size,size);
+ 					if (Math.abs(scale)<0.1){
+ 						action.turn = 1/action.turn;
+ 						ctx.scale(1,-1);
+ 					}
+ 					if (Math.abs(scale)>1){
+ 						action.turn = 1/action.turn;
+ 					}
+					ctx.scale(1, action.turn);
+					ctx.drawImage(img,-width/2,-height/2,width,height);
+				});
+ 			},
  			getStage : function(){
  				return this.stage;
+ 			},
+ 			convertToCanvasPoint : function(){
+ 				return {x:x+this.size-this.width/2,y:y+this.size-this.height/2};
  			}
  		};
 		var animation = {
-			resource : [],
+			resource : {
+				_length : 0,
+				push : function(id,res){
+					this[id] = res;
+					this._length++;
+				}
+			},
 			config : {},
 			sprites : [],
-			init : function(x,y,width,height){
+			pathList : [],
+			init : function(x,y,width,height,callback){
 				var $canvas = document.getElementById('myCanvas');
 				var $ctx = $canvas.getContext('2d');
 				$canvas.style.left = x || 0;
@@ -118,15 +171,34 @@
 				$canvas.width = width || window.innerWidth;
 				$canvas.height = height || window.innerHeight;
 				this.config = {x:x,y:y,width:width,height:height};
+				this.canvas = $canvas;
 				this.ctx = $ctx;
 				this.width = $canvas.width;
 				this.height = $canvas.height;
+				this.resourceList = {};
+				this.canvas.addEventListener('complete',callback,false);
 				this.state = "init";
 			},
 			load : function(){
-				for (var i=0,l=this.resource.length;i<l;++i) {
+				var resourceList = {};
+				var loaded = 0;
+				for (var id in this.resource) {
+					if (typeof this.resource[id] == 'function' || /^_/i.test(id)){
+						continue;
+					}
 					var img = new Image();
-					img.src = this.resource[i];
+					img.src = this.resource[id];
+					that = this;
+					(function(id){
+						img.onload = function(){
+							loaded++;
+							that.resourceList[id]={img:this,width:this.width/2,height:this.height/2};
+							if (loaded >= that.resource._length) {
+								var event = new Event('complete');
+								that.canvas.dispatchEvent(event);
+							}
+						}
+					})(id);
 				}
 			},
 			setSprite : function(img,x,y,width,height,action){
@@ -137,22 +209,21 @@
 				var action = action || {name:'stay'};
 				var sprite = new Sprite(img,x,y,width,height,action);
 				this.sprites.push(sprite);
-				this.resource.push(img);
 				return sprite;
 			},
 			addSpritesToStage : function(){
 				var ctx = this.ctx;
 				for (var i=0,l=this.sprites.length;i<l;i++){
 					var sprite = this.sprites[i];
+					that = this;
 					(function(sprite){
 						ctx.drawImage(sprite.canvas,sprite.x,sprite.y);
+						sprite.stage = that; 
 					})(sprite);
 				}
 			},
 			start : function(){
 				var ctx = this.ctx;
-				if (this.state != 'load')
-					this.load();
 				this.state = "start";
 				that = this;
 				function _onFrame(t){
@@ -179,45 +250,25 @@
 				return this.state;
 			},
 			autoClear : function(sprites){
-				for (var i=0,l=that.sprites.length;i<l;++i) {
+				for (var i=0,l=this.sprites.length;i<l;++i) {
 					var width = this.width;
 					var height = this.height;
-					var sprite = that.sprites[i];
-					var action = that.sprites[i].action;
-					if (sprite.y < -sprite.height){
+					var sprite = this.sprites[i];
+					var action = this.sprites[i].action;
+					if (sprite.y < -sprite.height || sprite.y > height){
 						if (action.autoRefresh) {
-							sprite.y = height;
-							sprite.x = Math.random()*width;
+							var indexP = Math.random()*this.pathList.length>>0;
+							sprite.x = this.pathList[indexP].x,
+							sprite.y = this.pathList[indexP].y;
 						}else{
-							sprites.splice(i,1);
-						}
-					}
-					if (sprite.y > height){
-						if (action.autoRefresh) {
-							sprite.y = -sprite.height;
-							sprite.x = Math.random()*width;
-						}else{
-							sprites.splice(i,1);
-						}
-					}
-					if (sprite.x < -sprite.width){
-						if (action.autoRefresh) {
-							sprite.x = width;
-							sprite.y = Math.random()*height;
-						}else{
-							sprites.splice(i,1);
-						}
-					}
-					if (sprite.x > width){
-						if (action.autoRefresh) {
-							sprite.x = -sprite.width;
-							sprite.x = Math.random()*height;
-						}else{
-							sprites.splice(i,1);
+							// sprites.splice(i,1);
 						}
 					}
 				}
-			}	
+			},
+			addPathPoint : function(p){
+				this.pathList.push(p);
+			}
 		};
 
 		animation.init();
