c9f369e7ab6e2902d1ba8a35d2cc6c6757077d6a
luzhuang <364439895@qq.com>
2015-02-11 08:26:24 +0800

    add Layer

--- js/animation.js
+++ js/animation.js
@@ -1,5 +1,5 @@
-;(function(window,document){
-	function init(e) {
+// ;(function(window,document){
+	// function init() {
 		var requestAnimationFrame = (function(){
 		   return  window.requestAnimationFrame       || 
 		           window.webkitRequestAnimationFrame || 
@@ -12,20 +12,20 @@
  		})();
 
  		function Sprite(img,x,y,width,height,action){
+ 			this.canvas = document.createElement('canvas');
+			this.ctx = this.canvas.getContext('2d');
  			this.x = this.originX = x;
  			this.y = this.originY = y;
  			this.width = this.originWidth = width;	
  			this.height = this.originHeight = height;
  			this.size = Math.sqrt(width*width+height*height);
- 			this.img = img;
- 			this.action = action;
- 			this.isTurn = false;
- 			this.stage = {};
- 			this.canvas = document.createElement('canvas');
  			this.canvas.width = this.size;
  			this.canvas.height= this.size;
-			this.ctx = this.canvas.getContext('2d');
+ 			this.img = img;
+ 			this.action = action || {name:'stay'};
+ 			this.layer = {};
  			this.init();
+ 			this.isTurn = false;
  		}
 
  		Sprite.prototype = {
@@ -42,6 +42,7 @@
 	 				ctx.translate(this.size/2,this.size/2);
 					ctx.drawImage(img,-width/2,-height/2,width,height);
 				}else{
+					// console.log(width);
 					ctx.drawImage(img,0,0,width,height);
 				}
  			},
@@ -52,26 +53,10 @@
  					width = this.width,
  					height = this.height,
  					ctx = this.ctx;
-				if (/down/i.test(actionName)){
+				
+				if (/func/i.test(actionName)){
 					this.y += action.vy;
-				}
-				if (/up/i.test(actionName)){
-					this.y -= action.vy;
-				}
-				if (/left/i.test(actionName)){
-					this.x -= action.vx;
-				}
-				if (/right/i.test(actionName)){
-					this.x += action.vx;
-				}
-				if (/wave/i.test(actionName)){
-					var delta = this.y*action.f/10;
-					var random = Math.random()*100>>0;
-					if (random % 45 ===0) {
-						action.swing = Math.random()-0.5+action.swing;
-					}
-					action.swing = action.swing % 2;
-					this.x += action.swing*Math.sin(delta);
+					this.x = action.func(this.y);
 				}
 				if (/rotate/i.test(actionName)){
 					ctx.clearRect(-size/2,-size/2,size,size);
@@ -92,14 +77,16 @@
 						this.action.p2 = {x:scale*300,y:150+100*scale};
 						this.action.t -= 1;
 					}else{ 
-						this.x = Math.pow((1-action.t),3)*p0.x+3*action.t*Math.pow((1-action.t),2)*(p0.x+p1.x)+3*Math.pow(action.t,2)*(1-action.t)*(p0.x+p2.x)+(p0.x+p3.x)*Math.pow(action.t,3);
-						this.y = Math.pow((1-action.t),3)*p0.y+3*action.t*Math.pow((1-action.t),2)*(p0.y+p1.y)+3*Math.pow(action.t,2)*(1-action.t)*(p0.y+p2.y)+(p0.y+p3.y)*Math.pow(action.t,3);
+						this.x = Math.pow((1-action.t),3)*p0.x+3*action.t*Math.pow((1-action.t),2)*(p0.x+p1.x)+
+								3*Math.pow(action.t,2)*(1-action.t)*(p0.x+p2.x)+(p0.x+p3.x)*Math.pow(action.t,3);
+						this.y = Math.pow((1-action.t),3)*p0.y+3*action.t*Math.pow((1-action.t),2)*(p0.y+p1.y)+
+								3*Math.pow(action.t,2)*(1-action.t)*(p0.y+p2.y)+(p0.y+p3.y)*Math.pow(action.t,3);
 					}
 				}
 				if (/transparent/i.test(actionName)){
 					var opacityStart = this.action.opacityStart,
 						allLength = opacityStart,
-						dest = this.stage.height - opacityStart;
+						dest = this.layer.height - opacityStart;
 					if (this.y > dest) {
 						var moveLength = this.y - dest;
 						var delta = moveLength/allLength;
@@ -172,111 +159,45 @@
 					ctx.drawImage(img,-width/2,-height/2,width,height);
 				});
  			},
- 			getStage : function(){
- 				return this.stage;
+ 			getLayer : function(){
+ 				return this.layer;
  			},
  			convertToCanvasPoint : function(){
  				return {x:x+this.size-this.width/2,y:y+this.size-this.height/2};
  			}
  		};
-		var animation = {
-			resource : {
-				_length : 0,
-				push : function(id,res){
-					this[id] = res;
-					this._length++;
-				}
+
+ 		function Layer(index){
+ 			this.canvas = document.createElement('canvas');
+			this.ctx = this.canvas.getContext('2d');
+ 			this.sprites  = [];
+ 			this.pathList = [];
+ 			this.index = index;
+ 			this.x = 0;
+ 			this.y = 0;
+ 			this.width = this.canvas.width = window.innerWidth;
+ 			this.height = this.canvas.height= window.innerHeight;
+ 		}
+
+ 		Layer.prototype = {
+ 			addSprite : function(sprite){
+ 				sprite.layer = this;
+ 				this.sprites.push(sprite);
+ 			},
+ 			addPathPoint : function(p){
+				this.pathList.push(p);
 			},
-			config : {},
-			sprites : [],
-			pathList : [],
-			init : function(x,y,width,height,callback){
-				var $canvas = document.getElementById('myCanvas');
-				var $ctx = $canvas.getContext('2d');
-				$canvas.style.left = x || 0;
-				$canvas.style.top = y || 0;
-				$canvas.width = width || window.innerWidth;
-				$canvas.height = height || window.innerHeight;
-				this.config = {x:x,y:y,width:width,height:height};
-				this.canvas = $canvas;
-				this.ctx = $ctx;
-				this.width = $canvas.width;
-				this.height = $canvas.height;
-				this.resourceList = {};
-				this.canvas.addEventListener('complete',callback,false);
-				this.state = "init";
-			},
-			load : function(){
-				var resourceList = {};
-				var loaded = 0;
-				for (var id in this.resource) {
-					if (typeof this.resource[id] == 'function' || /^_/i.test(id)){
-						continue;
-					}
-					var img = new Image();
-					img.src = this.resource[id];
-					that = this;
-					(function(id){
-						img.onload = function(){
-							loaded++;
-							that.resourceList[id]={img:this,width:this.width/2,height:this.height/2};
-							if (loaded >= that.resource._length) {
-								var event = new Event('complete');
-								that.canvas.dispatchEvent(event);
-							}
-						}
-					})(id);
-				}
-			},
-			setSprite : function(img,x,y,width,height,action){
-				var x = x || 0;
-				var y = y || 0;
-				var width  = width || this.width;
-				var height = height || this.height;
-				var action = action || {name:'stay'};
-				var sprite = new Sprite(img,x,y,width,height,action);
-				this.sprites.push(sprite);
-				return sprite;
-			},
-			addSpritesToStage : function(){
+			_addSpritesToLayer : function(){
 				var ctx = this.ctx;
 				for (var i=0,l=this.sprites.length;i<l;i++){
 					var sprite = this.sprites[i];
-					that = this;
+					var that = this;
 					(function(sprite){
 						ctx.drawImage(sprite.canvas,sprite.x,sprite.y);
-						sprite.stage = that; 
 					})(sprite);
 				}
 			},
-			start : function(){
-				var ctx = this.ctx;
-				this.state = "start";
-				that = this;
-				function _onFrame(t){
-					ctx.clearRect(0,0,that.width,that.height);
-					for (var i=0,l=that.sprites.length;i<l;++i) {
-						that.sprites[i].move();
-					}
-					that.autoClear(that.sprites);
-					that.addSpritesToStage();
-					if (that.state == "start") {
-						requestAnimationFrame(_onFrame);
-					}
-				}
-				requestAnimationFrame(_onFrame);
-			},
-			stop : function(){
-				this.state = "stop";
-			},
-			clear : function(){
-				this.state = "over"
-				this.ctx.clearRect(0,0,this.width,this.height);
-			},
-			getState : function(){
-				return this.state;
-			},
-			autoClear : function(sprites){
+			_autoClear : function(sprites){
 				for (var i=0,l=this.sprites.length;i<l;++i) {
 					var width = this.width;
 					var height = this.height;
@@ -292,13 +213,90 @@
 					}
 				}
 			},
-			addPathPoint : function(p){
-				this.pathList.push(p);
+			_onFrame : function(){
+				this.ctx.clearRect(0,0,this.width,this.height);
+				for (var i=0,l=this.sprites.length;i<l;++i) {
+					this.sprites[i].move();
+				}
+				this._autoClear(this.sprites);
+				this._addSpritesToLayer();
+			}
+ 		};
+
+ 		function Scene(x,y,width,height){
+			this.canvas = document.getElementById('myCanvas');
+			this.ctx = this.canvas.getContext('2d');
+			this.x = x;
+			this.y = y;
+			this.width = this.canvas.width = width;
+			this.height = this.canvas.height =height;
+			this.layers = [];
+			this.state = "init";
+ 		}
+
+		Scene.prototype = {
+			addLayer : function(layer){
+				layer.width = layer.canvas.width = this.width;
+				this.layers[layer.index] = layer;
+			},
+			start : function(){
+				var ctx = this.ctx;
+				this.state = "start";
+				var that = this;
+				function _onFrame(){				
+					if (that.state == "start") {
+						ctx.clearRect(0,0,that.width,that.height);
+						for (var index in that.layers){
+							var layer = that.layers[index]
+							layer._onFrame();
+							ctx.drawImage(layer.canvas,0,0);
+						}
+						requestAnimationFrame(_onFrame);
+					}
+				}
+				requestAnimationFrame(_onFrame);
+			},
+			stop : function(){
+				this.state = "stop";
+			},
+			clear : function(){
+				this.state = "over"
+				this.ctx.clearRect(0,0,this.width,this.height);
+			},
+			getState : function(){
+				return this.state;
 			}
 		};
+		
+		function LoadQueue(){
+			this.resourceNum = 0;
+			this.resourceList = {};
+		}
 
-		animation.init();
-		window.animation = animation;
-	}
-	window.addEventListener('load',init,false);
-})(window,document);
+		LoadQueue.prototype = {
+			addResource : function(id,res){
+				this.resourceList[id] = res;
+				this.resourceNum++;
+			},
+			load : function(callback){
+				var loaded = 0;
+				var res = {};
+				for (var id in this.resourceList) {
+					var img = new Image();
+					img.src = this.resourceList[id];
+					res[id] = img;
+					var that = this;
+					(function(id){
+						img.onload = function(){
+							loaded++;
+							if (loaded == that.resourceNum) {
+								callback(res);
+							}
+						}
+					})(id);
+				}
+			}
+		}
+	// }
+	// window.addEventListener('load',init,false);
+// })(window,document);
